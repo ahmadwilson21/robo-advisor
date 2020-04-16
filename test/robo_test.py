88f4bdd854d4bf7cdc6de5f2432aed8a070ba62c
ahmadwilson21 <50254137+ahmadwilson21@users.noreply.github.com>
2020-04-15 20:39:14 -0400

    Fixed example reports

--- test/robo_test.py
+++ test/robo_test.py
@@ -92,14 +92,7 @@ def test_write_to_csv():
         {"timestamp": "2019-06-01", "open": '99.2798',  "high": "100.8600", "low": "99.1700",  "close": "100.7900", "volume": "28655624"}
     ]
 
-    example_rows_wrong = [
-        {"timestamp": "2019-06-01", "open": "101.0924", "high": "101.9500", "low": "100.5400", "close": "101.6300", "volume": "22165128"},
-        {"timestamp": "2019-06-07", "open": "102.6500", "high": "102.6900", "low": "100.3800", "close": "100.8800", "volume": "28232197"},
-        {"timestamp": "2019-06-06", "open": "102.4800", "high": "102.6000", "low": "101.9000", "close": "102.4900", "volume": "21122917"},
-        {"timestamp": "2019-06-05", "open": "102.0000", "high": "102.3300", "low": "101.5300", "close": "102.1900", "volume": "23514402"},
-        {"timestamp": "2019-06-04", "open": "101.2600", "high": "101.8600", "low": "100.8510", "close": "101.6700", "volume": "27281623"},
-        {"timestamp": "2019-06-01", "open": '99.2798',  "high": "100.8600", "low": "99.1700",  "close": "100.7900", "volume": "28655624"}
-    ]
+   
 
     csv_filepath = os.path.join(os.path.dirname(__file__), "example_reports", "temp_prices.csv")
 
